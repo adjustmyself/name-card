bca129b52af0535123e4831e01ad74ca78e68c0a
adjustmyself <adjust_myself@hotmail.com>
2020-01-03 16:50:39 +0800

    Updates

---
+++ precache-manifest.72cfc9ffc2302948a4de770b086a6dc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd9ff57b4423e72bf4a72be8d1bb797",
+    "url": "/name-card/index.html"
+  },
+  {
+    "revision": "28bc27116419bcc75636",
+    "url": "/name-card/static/css/main.412f68a9.chunk.css"
+  },
+  {
+    "revision": "1069408d6f4e5b2d4853",
+    "url": "/name-card/static/js/2.e7759007.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/name-card/static/js/2.e7759007.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28bc27116419bcc75636",
+    "url": "/name-card/static/js/main.6ae6a713.chunk.js"
+  },
+  {
+    "revision": "37a3fb9d25c6b2a02e84",
+    "url": "/name-card/static/js/runtime-main.6120484d.js"
+  },
+  {
+    "revision": "16f2a9b93e7bd49858ca0fab7cbcc484",
+    "url": "/name-card/static/media/bg.16f2a9b9.jpg"
+  },
+  {
+    "revision": "d5114996d50db1a5fcffff8f7b9e0bd4",
+    "url": "/name-card/static/media/github-logo2.d5114996.png"
+  },
+  {
+    "revision": "07b1320944b0d443ac5dd91102c42240",
+    "url": "/name-card/static/media/homePage-reduce.07b13209.png"
+  },
+  {
+    "revision": "5a5616891668ab377d4b378f0414b3f7",
+    "url": "/name-card/static/media/ig-logo.5a561689.png"
+  },
+  {
+    "revision": "691a76ac2fc4922de2daf051a60b822e",
+    "url": "/name-card/static/media/personal-2.691a76ac.png"
+  }
+]);
